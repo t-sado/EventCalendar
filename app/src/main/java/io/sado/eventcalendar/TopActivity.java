9a3ed23fa475848bf7ac4f5d1a1378cc8be01562
sado <sado.tomoki@no1s.biz>
2018-10-29 09:39:19 +0900

    2018/10/29 first commit

---
+++ app/src/main/java/io/sado/eventcalendar/TopActivity.java
@@ -0,0 +1,43 @@
+package io.sado.eventcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class TopActivity extends AppCompatActivity {
+    private TextView titleText;
+    private Button prevButton, nextButton;
+    private CalendarAdapter mCalendarAdapter;
+    private GridView calendarGridView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+
+        titleText = findViewById(R.id.titleText);
+        prevButton = findViewById(R.id.prevButton);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.prevMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCalendarAdapter.nextMonth();
+                titleText.setText(mCalendarAdapter.getTitle());
+            }
+        });
+        calendarGridView = findViewById(R.id.calendarGridView);
+        mCalendarAdapter = new CalendarAdapter(this);
+        calendarGridView.setAdapter(mCalendarAdapter);
+        titleText.setText(mCalendarAdapter.getTitle());
+    }
+}
